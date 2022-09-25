4b2f61aec96b7e139db3f05fa571fa6e21817fb4
Declan Davis <dexthefish@gmail.com>
2022-09-25 13:27:36 -0400

    adjust suit pictures

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ export default function Card(props) {
     <div className="card">
       <div className="suit">
         the suit is: {props.suit}
-        <img src={'hearts.png'} alt='spades icon' size='100' width='100' />
+        <img src={'spades.png'} alt='spades icon' size='100' width='100' />
       </div>
       <div className="rank">
         the rank is: {props.rank}
